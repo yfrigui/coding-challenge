a784a6cf545b8f1333ceefb375b6ef99c3f569ae
yasmine <35513348+yfrigui@users.noreply.github.com>
2019-03-29 16:17:53 -0700

    added table of links and functions for links

--- referralsite/links/models.py
+++ referralsite/links/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Link(models.Model):
-	title = models.CharField(max_length=20)
+	title = models.CharField(max_length=20, unique=True)
 	clicks = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.title
+
+	def visit(self):
+		self.clicks = self.clicks + 1
+
+	def edit(self, new_title):
+		self.title = new_title
 
 
 
--- referralsite/links/views.py
+++ referralsite/links/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Link
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Grow the web with referrals!")
+	links = Link.objects.order_by('-clicks')
+	context = {'links': links,}
+	return render(request, 'links/index.html', context)
 
 def click(request):
 	link_title = request.GET.get('link')
+	try:
+		link = Link.objects.get(title=link_title)
+	except Link.DoesNotExist:
+		raise Http404("Link does not exist")
+	link.visit()
 	return HttpResponse("You will be redirected to %s." % link_title)
