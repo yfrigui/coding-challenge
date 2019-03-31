c5af8a064a3f58bec27b2154a600722381218a8a
yasmine <35513348+yfrigui@users.noreply.github.com>
2019-03-30 19:23:13 -0700

    updated readme

--- referralsite/links/views.py
+++ referralsite/links/views.py
@@ -24,7 +24,7 @@ def add_link(request):
 			link = Link(title=link_title)
 			try:
 				link.save()
-			except:
+			except: #go back if user tries to add a link that already exists
 				links = Link.objects.order_by('-clicks')
 				return render(request, 'links/index.html', {'links':links})
 			links = Link.objects.order_by('-clicks')
@@ -42,7 +42,7 @@ def click(request):
 		link = Link.objects.get(title=link_title)
 	except Link.DoesNotExist:
 		raise Http404("Link does not exist")
-	link.visit()
+	link.visit() #increments link count
 	return HttpResponse("You will be redirected to %s." % link_title)
 
 def delete_link(request):
@@ -62,38 +62,29 @@ def begin_edit_link(request):
 		link = Link.objects.get(title=link_title)
 	except Link.DoesNotExist:
 		raise Http404("Link does not exist")
-	#links = Link.objects.order_by('-clicks')
 	context = {'link': link}
 	return render(request, 'links/edit_link.html', context)
 
 def finish_edit_link(request):
 	link_title = request.GET.get('link')
-	print("editing: " + link_title)
 	try:
 		link = Link.objects.get(title=link_title)
 	except Link.DoesNotExist:
 		raise Http404("Link does not exist")
 	# if this is a POST request we need to process the form data
 	if request.method == 'POST':
-		print("request method is post")
 		# create a form instance and populate it with data from the request:
 		form = EditLinkForm(request.POST)
-		#print("form cleaned data: " + form.new_link_title)
 		# check whether it's valid:
-		print(form.errors)
 		if form.is_valid():
-			print("form is valid")
 			new_link_title = form.cleaned_data['new_link_title']
 			link.title = new_link_title
-			print("new name is " + new_link_title)
 			try:
 				link.save()
-			except:
+			except: #return if a link already exists with this name
 				links = Link.objects.order_by('-clicks')
 				return render(request, 'links/index.html', {'links':links})
 			links = Link.objects.order_by('-clicks')
 			return render(request, 'links/index.html', {'links':links})
-            #return HttpResponseRedirect('/thanks/')
-    # if a GET (or any other method) we'll create a blank form
 	links = Link.objects.order_by('-clicks')
 	return render(request, 'links/index.html', {'links': links})	
